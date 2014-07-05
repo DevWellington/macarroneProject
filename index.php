f2519de2f83c47667ac3e0aa74d94e0bac99274a
Wellington Ribeiro <devwellington@gmail.com>
2014-07-05 00:52:18 -0300

    Removed function - use array_walk to require

--- index.php
+++ index.php
@@ -13,52 +13,27 @@
 
           <?php 
 
-            function IncludeFilesOfRoute($_path, $_arRoutes){
-              
-              $filePath = './includes/';
-
-              if (!file_exists($filePath.$_path.'.php') && ($_path !== '')){
-                require_once $filePath.'404.php';
-              } else {
-
-                foreach ($_arRoutes as $key => $value) {
-
-                  if ($_path == $value){
-                    require_once $filePath. $_path.'.php';
-                    break;
-                  } elseif ($_path == ''){
-                    require_once $filePath.'home.php';
-                    break;
-                  }
-                }
-              }
-            }
-
             $arRoutes = ['home', 'empresa', 'produtos', 'servicos', 'contato'];
-            // $filePath = './includes/';
 
             $route = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
             $path = explode('/', $route['path'])[1];
 
-            IncludeFilesOfRoute($path, $arRoutes);
+            array_walk($arRoutes, function ($x) use ($path){
 
+              $filePath = './includes/';
+              
+              if (!file_exists($filePath.$path.'.php') && ($path !== '')){
+                require_once $filePath.'404.php';
+              } else {              
 
-/*
-            if (!file_exists($filePath.$path.'.php') && ($path !== '')){
-              require_once './includes/404.php';
-            } else {
-
-              foreach ($arRoutes as $key => $value) {
-
-                if ($path == $value){
+                if ($x == $path){
                   require_once $filePath. $path.'.php';
-                  break;
                 } elseif ($path == ''){
                   require_once $filePath.'home.php';
-                  break;
                 }
+
               }
-            }*/
+            });
 
           ?>
 
