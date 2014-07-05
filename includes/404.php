1e44cee6bf5824fcb6dcdb52779b5fac32152a62
Wellington Ribeiro <devwellington@gmail.com>
2014-07-05 00:40:39 -0300

    Include file 404.php - Page not found

---
+++ includes/404.php
@@ -0,0 +1,10 @@
+<?php header('HTTP/1.0 404 Not Found') ?>
+
+		  <div class="inner cover">
+            <h1 class="cover-heading">Page not found</h1>
+
+            <div class="row featurette">
+		            <h2 class="featurette-heading">Error Code: 404</h2>
+	        </div>
+	        
+          </div>
